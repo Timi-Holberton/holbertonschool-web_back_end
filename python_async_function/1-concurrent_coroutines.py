560fb81f8d574bb8bfacb0c62fac78885dba1c61
Timi-Holberton <timi.holberton@gmail.com>
2025-07-29 14:21:42 +0200

    Correction des noms de variables dans la fonction wait_n pour améliorer la clarté et la cohérence du code.

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -9,7 +9,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(nombre: int, delai_max: int) -> List[float]:
+async def wait_n(n: int, delai_max: int) -> List[float]:
     """
     Exécute la coroutine wait_random un nombre donné de fois avec un délai
     maximal spécifié, puis retourne la liste des délais obtenus, triée
@@ -24,16 +24,16 @@ async def wait_n(nombre: int, delai_max: int) -> List[float]:
         List[float] : liste des délais dans l’ordre d’achèvement
     """
     # Crée une liste de coroutines prêtes à être lancées
-    listCoroutine = [wait_random(delai_max) for _ in range(nombre)]
+    listCoroutine = [wait_random(delai_max) for _ in range(n)]
 
     # Planifie l’exécution concurrente avec asyncio.create_task
-    tache = [asyncio.create_task(coro) for coro in listCoroutine]
+    tache_repet = [asyncio.create_task(coro) for coro in listCoroutine]
 
     # Liste des délais dans l’ordre où les tâches se terminent
     delais_trier = []
 
     # Récupère les résultats dans l’ordre d’achèvement des tâches
-    for tache_fin in asyncio.as_completed(tache):
+    for tache_fin in asyncio.as_completed(tache_repet):
         delai = await tache_fin
         delais_trier.append(delai)
 
