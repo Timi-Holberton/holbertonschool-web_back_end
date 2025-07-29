51a63242edc49d714fd77c299d345fe2d14399ff
Timi-Holberton <timi.holberton@gmail.com>
2025-07-29 13:49:53 +0200

    Ajout d'une docstring à la fonction asynchrone wait_n pour décrire son fonctionnement et ses arguments

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,17 +1,30 @@
 #!/usr/bin/env python3
+""" Lance une coroutine"""
 import asyncio, random
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Lance n fois la coroutine wait_random avec un délai maximal donné et
+    retourne une liste des délais obtenus, ordonnée dans l'ordre d'arrivée
+    (croissant), sans utiliser la fonction sort().
 
+    Args:
+        n (int): Le nombre de fois à exécuter wait_random.
+        max_delay (int): Le délai maximal (en secondes) pour chaque attente.
+
+    Returns:
+        List[float]: Liste des délais flottants dans l'ordre d'achèvement.
+    """
     tasks = [wait_random(max_delay) for _ in range(n)]
     results = []
     for task in asyncio.as_completed(tasks):
         delay = await task
         results.append(delay)
     return results
+
 
 
 
