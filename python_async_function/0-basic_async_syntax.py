b513a844d463cca9b19bc28a411efe6f972c1689
Timi-Holberton <timi.holberton@gmail.com>
2025-07-29 10:41:30 +0200

    Ajout de la fonction asynchrone wait_random avec gestion d'un délai aléatoire

---
+++ python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Fonction qui écrit une coroutine asynchrone qui prend en argument un entier
+(max_delay, avec une valeur par défaut de 10) nommé wait_random qui attend
+un délai aléatoire compris entre 0 et max_delay (inclus et valeur flottante)
+secondes et le renvoie finalement.
+"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    value = random.uniform(0, max_delay)
+    await asyncio.sleep(value)
+    return value
