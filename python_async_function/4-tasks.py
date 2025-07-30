0aa341b649ac358c185606e473752d96928c3c6e
Timi-Holberton <timi.holberton@gmail.com>
2025-07-30 10:24:56 +0200

    Ajout d'une docstring à la fonction task_wait_n pour clarifier son objectif

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -9,7 +9,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-
+    """ Reprise sur exercice précédent"""
     listCoroutine = [wait_random(max_delay) for _ in range(n)]
     tache_repet = [asyncio.create_task(coro) for coro in listCoroutine]
     delais_trier = []
