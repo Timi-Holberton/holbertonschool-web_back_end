bad4fe821aee428da8c9ca9d7abcfd3d9f4b416b
Timi-Holberton <timi.holberton@gmail.com>
2025-08-01 09:01:37 +0200

    Refactor la docstring de la coroutine async_comprehension pour clarifier la collecte de nombres flottants et simplifier le code en utilisant une compréhension asynchrone.

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -12,14 +12,11 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """
-    Collecte 10 nombres à virgule flottante aléatoires à l'aide d'une
-    compréhension asynchrone sur le générateur asynchrone.
+    Collecter 10 nombres flottants générés de manière asynchrone.
 
-    Return : Liste[float] : une liste contenant 10 nombres à virgule flottante
-    aléatoires.
+    Returns:
+        List[float]: Une liste de 10 nombres à virgule flottante générés
+        de façon asynchrone.
     """
-    coroutine = []                          # Liste vide
-    async for value in async_generator():   # boucle sur async_generator
-        coroutine.append(value)             # ajoute calue à coroutine
-        if len(coroutine) == 10:            # vérifie qu'il y en ai 10
-            return coroutine                # return si condition atteinte
+
+    return [value async for value in async_generator()]
