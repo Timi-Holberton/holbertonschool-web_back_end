88904eaea16e6a62e56d32341be0af83547f9931
Timi-Holberton <timi.holberton@gmail.com>
2025-08-01 08:44:59 +0200

    Ajout de la coroutine async_comprehension pour collecter 10 nombres aléatoires à l'aide d'une compréhension asynchrone.

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Importez async_generator depuis la tâche précédente, puis écrivez une
+coroutine appelée async_comprehension qui ne prend aucun argument.
+
+La coroutine collectera 10 nombres aléatoires à l'aide d'une compréhension
+asynchrone sur async_generator, puis renverra les 10 nombres aléatoires.
+"""
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """  """
+    coroutine = [] # Liste vide
+    async for value in async_generator():   # boucle sur async_generator
+        coroutine.append(value)             # ajoute calue à coroutine
+        if len(coroutine) == 10:            # vérifie qu'il y en ai 10
+            return coroutine                # return si condition atteinte
