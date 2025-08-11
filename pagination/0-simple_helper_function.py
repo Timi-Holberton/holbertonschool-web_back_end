129c592d210b9ea3997200b93f8760aedbf3c9a8
Timi-Holberton <timi.holberton@gmail.com>
2025-08-11 10:35:53 +0200

    Ajout des annotations de type à la fonction index_range pour améliorer la clarté du code

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+from typing import Tuple
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> Tuple:
     """
     """
     star_index = (page - 1) * page_size
