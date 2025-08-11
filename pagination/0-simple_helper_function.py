b398a80d49a740442784c35076073c49a50370a5
Timi-Holberton <timi.holberton@gmail.com>
2025-08-11 10:43:39 +0200

    Modification de la fonction index_range pour retourner une liste d'entiers au lieu d'un tuple, améliorant ainsi la clarté des types.

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-from typing import Tuple
+from typing import List
 
-def index_range(page: int, page_size: int) -> Tuple:
+
+def index_range(page: int, page_size: int) -> List[int]:
     """
+    La fonction doit retourner un tuple de deux éléments contenant un indice
+    de début et un indice de fin correspondant à la plage d’index à retourner
+    dans une liste pour ces paramètres de pagination spécifiques.
     """
     star_index = (page - 1) * page_size
     fin_index = star_index + page_size
