1b68d979367affd670ee98fa8897396881403090
Timi-Holberton <timi.holberton@gmail.com>
2025-08-11 19:26:13 +0200

    Ajout de la documentation pour la méthode get_hyper_index afin de clarifier son fonctionnement et ses types de retour.

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -40,6 +40,24 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
+        """
+        Module de pagination avec index hypermédia.
+
+        Ce module fournit des fonctionnalités permettant de naviguer dans un
+        jeu de données de manière paginée, avec gestion des index pour
+        l'accès direct à un élément et à la page suivante.
+
+        Fonctions principales :
+        - get_page(page, page_size) : Retourner une page spécifique de données.
+        - get_hyper_index(index, page_size) : Retourner un dictionnaire contenant
+        les données de la page correspondant à l'index donné, ainsi que
+        l'index de la page suivante si disponible.
+
+        Types :
+        - Les pages sont renvoyées sous forme de listes d’éléments.
+        - Les résultats hypermédia sont renvoyés sous forme de dictionnaires
+        avec les clés : 'index', 'data', 'page_size', 'next_index'.
+        """
         assert isinstance(index, int) or index is None
         assert isinstance(page_size, int) and page_size > 0
 
