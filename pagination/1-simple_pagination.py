1b9310cee8685ed721b6526b018d82eb9732d50e
Timi-Holberton <timi.holberton@gmail.com>
2025-08-11 14:42:51 +0200

    Refactor la fonction index_range en la déplaçant hors de la classe Server pour améliorer la modularité et la clarté du code.

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -6,6 +6,18 @@ import math
 from typing import List
 
 
+def index_range(page: int, page_size: int) -> Tuple:
+    """
+    La fonction doit retourner un tuple de deux éléments contenant un
+    indice de début et un indice de fin correspondant à la plage d’index
+    à retourner dans une liste pour ces paramètres de pagination
+    spécifiques.
+    """
+    start_index = (page - 1) * page_size
+    fin_index = start_index + page_size
+    return (start_index, fin_index)
+
+
 class Server:
     """Server class to paginate a database of popular baby names.
     """
@@ -25,28 +37,16 @@ class Server:
 
         return self.__dataset
 
-    @staticmethod
-    def index_range(page: int, page_size: int) -> Tuple:
-        """
-        La fonction doit retourner un tuple de deux éléments contenant un
-        indice de début et un indice de fin correspondant à la plage d’index
-        à retourner dans une liste pour ces paramètres de pagination
-        spécifiques.
-        """
-        start_index = (page - 1) * page_size
-        fin_index = start_index + page_size
-        return (start_index, fin_index)
-
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """
+        Retourne une portion du dataset correspondant à la page demandée.
         """
         # Vérification des paramètres
-        assert isinstance(page, int) and page > 0, "page doit être un entier"
-        assert isinstance(page_size, int) and page_size > 0,
-        "page_size doit être un entier"
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
 
         # Calcul des indices de début et fin
-        start_index, end_index = self.index_range(page, page_size)
+        start_index, end_index = index_range(page, page_size)
 
         # Récupération des données
         dataset = self.dataset()
