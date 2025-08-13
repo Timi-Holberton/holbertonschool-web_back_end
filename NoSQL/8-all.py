31a41bf352b914c3a7cee9d592c1d9100ea8ceee
Timi-Holberton <timi.holberton@gmail.com>
2025-08-13 07:40:56 +0200

    Ajout de la documentation pour la fonction list_all afin de clarifier son objectif

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,5 +1,9 @@
 """
+Écrivez une fonction Python qui répertorie tous les documents
+d'une collection.
 """
 
+
 def list_all(mongo_collection):
+    """ Liste tous les doc """
     return list(mongo_collection.find())
