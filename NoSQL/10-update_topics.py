a03b56b920ff57ab5ba55796b67553b57447ae55
Timi-Holberton <timi.holberton@gmail.com>
2025-08-13 08:54:38 +0200

    Correction de l'argument 'multi' dans la fonction update_topics pour permettre la mise à jour de plusieurs documents

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -25,5 +25,6 @@ def update_topics(mongo_collection, name, topics):
     """
     return mongo_collection.update(
         {"name": name},
-        {"$set": {"topics": topics}}
+        {"$set": {"topics": topics}},
+        {"multi": True}
     )
