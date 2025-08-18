ac159a12d49d1076732ef67881547362121863c1
Timi-Holberton <timi.holberton@gmail.com>
2025-08-18 14:23:19 +0200

    Réécriture de la fonction add pour utiliser la syntaxe fléchée ES6

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,26 @@
+// Réécrivez la fonction standard suivante afin d'utiliser la syntaxe
+// fléchée ES6 de la fonction add (il s'agira ensuite d'une fonction anonyme).
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  this.addNeighborhood = (newNeighborhood) => {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return this.sanFranciscoNeighborhoods;
+  };
+}
+/*
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = function add(newNeighborhood) {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
+*/
+// Permet de supprimer "const self = this" car grâce à la fonction fléchés
+// le "this" reste celui du parent.
+
+// on supprime "function add" et on ajoute "=>", elle devient une
+// fonction anonyme.
