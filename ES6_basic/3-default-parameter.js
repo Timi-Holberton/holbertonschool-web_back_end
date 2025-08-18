1f1bd1475607402b26f7c5affdaeda76f6f0d60c
Timi-Holberton <timi.holberton@gmail.com>
2025-08-18 16:18:42 +0200

    Condense la fonction getSumOfHoods en utilisant des paramètres par défaut

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -1,3 +1,22 @@
+// Condensez le contenu de la fonction suivante en une seule ligne,
+// sans modifier le nom des fonctions/variables.
+
+// Les valeurs par default sont indiquées directement dans la définition des
+// paramètres.
+
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
+
+/*
+export default function getSumOfHoods(initialNumber, expansion1989, expansion2019) {
+  if (expansion1989 === undefined) {
+    expansion1989 = 89;
+  }
+
+  if (expansion2019 === undefined) {
+    expansion2019 = 19;
+  }
+  return initialNumber + expansion1989 + expansion2019;
+}
+*/
