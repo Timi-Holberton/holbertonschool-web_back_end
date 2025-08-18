c9e4fd1fe9cc728ba94767fe902ac8529b00c03a
Timi-Holberton <timi.holberton@gmail.com>
2025-08-18 16:15:22 +0200

    Aucune modification apportée au code

--- ES6_basic/3-default-parameter.js
+++ ES6_basic/3-default-parameter.js
@@ -2,9 +2,8 @@
 // sans modifier le nom des fonctions/variables.
 // Indice : la clé ici est de définir les valeurs par défaut des paramètres
 // de la fonction.
+// les valeurs par default sont indiquées directement dans la définition des
+// paramètres.
 export default function getSumOfHoods(initialNumber, expansion1989 = 89, expansion2019 = 19) {
   return initialNumber + expansion1989 + expansion2019;
 }
-
-// les valeurs par default sont indiquées directement dans la définition des
-// paramètres.
