0724609fca9ce49f553193dfc7cbcd813915a878
Timi-Holberton <timi.holberton@gmail.com>
2025-08-18 17:04:14 +0200

    Utilisation de la syntaxe abrégée pour l'objet budget dans la fonction getBudgetObject

---
+++ ES6_basic/7-getBudgetObject.js
@@ -0,0 +1,19 @@
+// Remarquez que les clés et les noms de variables sont identiques.
+// Modifiez l'objet budget de la fonction suivante afin d'utiliser
+// simplement la syntaxe abrégée de la propriété de l'objet à la place.
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {income, gdp, capita};
+  return budget;
+}
+
+/*
+export default function getBudgetObject(income, gdp, capita) {
+  const budget = {
+    income: income,
+    gdp: gdp,
+    capita: capita,
+  };
+
+  return budget;
+}
+*/
