c82b0caf585357aed9506d0da82e9e808eb6719e
Timi-Holberton <timi.holberton@gmail.com>
2025-08-18 17:10:18 +0200

    Aucune modification apportée au code

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,6 +1,3 @@
-// Remarquez que les clés et les noms de variables sont identiques.
-// Modifiez l'objet budget de la fonction suivante afin d'utiliser
-// simplement la syntaxe abrégée de la propriété de l'objet à la place.
 export default function getBudgetObject(income, gdp, capita) {
   const budget = { income, gdp, capita };
   return budget;
