0781b5e2e37c3a6c7b0815caa119fbda5b344729
Timi-Holberton <timi.holberton@gmail.com>
2025-08-19 10:56:10 +0200

    Aucune modification apportée à la fonction createReportObject

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,12 +1,15 @@
 export default function createReportObject(employeesList) {
   return {
-    allEmployees: {     // clé allEmployees → contiendra tous les départements
-      ...employeesList, // avec le spread on extrait directement les clés/valeurs de employeesList
-    },                  // ... agit cmme un copier coller des propriétés internes de employeesList dans allEmployees
+    allEmployees: { // clé allEmployees → contiendra tous les départements
+      ...employeesList, // avec ... on extrait les clés/valeurs de employeesList
+    }, // agit cmme un copier coller des propriétés internes de employeesList
+    // dans allEmployees
     // ES6 → pas besoin du mot-clé function
     getNumberOfDepartments(employeesList) {
-      return Object.keys(employeesList).length; // Object.keys(...) renvoie toutes les clés de l'objet employeesList.
-// .length sur un tableau donne le nombre d’éléments dans ce tableau, ici il renvoie le nbre de département dans employeesList
+      return Object.keys(employeesList).length;
+      // Object.keys(...) renvoie toutes les clés de l'objet employeesList.
+      // .length sur un tableau donne le nombre d’éléments dans ce tableau,
+      // ici il renvoie le nbre de département dans employeesList
     },
   };
 }
