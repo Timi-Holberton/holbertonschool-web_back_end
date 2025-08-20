2983b34f961234565ae4d67d7d9c701ed5ad64bf
Timi-Holberton <timi.holberton@gmail.com>
2025-08-20 18:21:50 +0200

    implémentation de la classe abstraite Building avec vérification de la méthode evacuationWarningMessage dans les classes enfants

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,14 +1,14 @@
 export default class Building {
   constructor(sqft) {
-    if (new.target === Building) {
-      throw new Error("Cannot instantiate abstract class Building directly");
-    }
     this._sqft = sqft;
 
-    // Vérification que la méthode est bien définie dans la classe enfant
-    if (this.evacuationWarningMessage === undefined ||
-        typeof this.evacuationWarningMessage !== "function") {
-      throw new Error("Class extending Building must override evacuationWarningMessage");
+    // Si on est dans une classe fille (pas directement Building)
+    // alors on vérifie la présence de evacuationWarningMessage
+    if (new.target !== Building) {
+      if (this.evacuationWarningMessage === undefined ||
+          typeof this.evacuationWarningMessage !== "function") {
+        throw new Error("Class extending Building must override evacuationWarningMessage");
+      }
     }
   }
 
