ea0486cb58f7782ea3b83d537729eff381178912
Timi-Holberton <timi.holberton@gmail.com>
2025-08-20 17:02:06 +0200

    Ajout de la classe Currency avec des accesseurs et des mutateurs pour le code et le nom

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,25 @@
+export class Currency {
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+  get code() {
+    return this._code;
+  }
+
+  set code(newcode) {
+    this._code = newcode;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    this._name = newName;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
