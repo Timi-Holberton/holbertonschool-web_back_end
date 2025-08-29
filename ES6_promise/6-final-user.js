7d2ce7da7e88c8ea612b75aec3e2294113699619
Timi-Holberton <timi.holberton@gmail.com>
2025-08-29 09:02:48 +0200

    delete code en trop

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,11 @@
+// 6-final-user.js
 import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
+  // On utilise Promise.allSettled pour attendre les 2 promesses
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]).then((results) =>
-    results.map((entry) => ({
-      status: entry.status,
-      value: entry.value || entry.reason,
-    }))
-  );
+  ]);
 }
