171ea6febca9d7be79821f9094b1dd909d63d40e
Timi-Holberton <timi.holberton@gmail.com>
2025-09-01 10:59:27 +0200

    utilisation de process.stdin.on et stdout

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,4 +1,15 @@
-const readline = require('node:readline');
+console.log(`Welcome to Holberton School, what is your name?`);
+process.stdin.on('data', data => {
+  console.log(`Your name is: ${data.toString().trim()}`);
+});
+process.stdin.on('end', function() {
+  process.stdout.write(`This important software is now closing\n`);
+});
+
+
+
+
+/*const readline = require('node:readline');
 
 const lireligne = readline.createInterface({
   input: process.stdin,
@@ -7,5 +18,7 @@ const lireligne = readline.createInterface({
 
 lireligne.question(`Welcome to Holberton School, what is your name?\n`, name => {
   console.log(`Your name is: ${name}`);
-  lireligne.close(`This important software is now closing\n`);
+  console.log(`This important software is now closing`)
+  lireligne.close();
 });
+*/
