48ea86ced76630e31ecdac8868f5555b70142a4e
Timi-Holberton <timi.holberton@gmail.com>
2025-09-01 11:13:47 +0200

    modif pour eslint

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,24 +1,7 @@
-console.log(`Welcome to Holberton School, what is your name?`);
-process.stdin.on('data', data => {
+console.log('Welcome to Holberton School, what is your name?');
+process.stdin.on('data', (data) => {
   console.log(`Your name is: ${data.toString().trim()}`);
 });
-process.stdin.on('end', function() {
-  process.stdout.write(`This important software is now closing\n`);
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
-
-
-
-
-/*const readline = require('node:readline');
-
-const lireligne = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-lireligne.question(`Welcome to Holberton School, what is your name?\n`, name => {
-  console.log(`Your name is: ${name}`);
-  console.log(`This important software is now closing`)
-  lireligne.close();
-});
-*/
